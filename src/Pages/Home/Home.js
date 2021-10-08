501cd89040484fc8b72f7094e11f5bae388c45ed
Arnab Datta <dattaarnab2013@gmail.com>
2021-10-09 02:26:08 +0530

    RESPONSIVE

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -23,142 +23,112 @@ function Home() {
       <div className="HomePageWrap">
         <div className="HomePage center-flex-class">
           <div className="profileDescDiv">
-            <div className="profileDescPart leftSec center-flex-class">
-              <div className="leftSecInner">
-                <div className="sideClass">
-                  <div className="subCLs svg1">
-                    <div className="imgPar m center-flex-class">
-                      <img src={developer_activity} alt="developer" />
-                    </div>
-                  </div>
-                  <div className="subCLs svg2">
-                    <div className="imgPar m center-flex-class">
-                      <img src={solution_mindset} alt="solution_mindset" />
-                    </div>
-                  </div>
-                  <div className="subCLs svg3">
-                    <div className="imgPar m center-flex-class">
-                      <img src={Mobile_application} alt="Mobile_application" />
-                    </div>
-                  </div>
+            {/* <div className="profileDescPart leftSec center-flex-class"> */}
+            {/* <div className="leftSecInner"> */}
+            <div className="profileDescPart sideClass">
+              <div className="subCLs svg1">
+                <div className="imgPar m center-flex-class">
+                  <img src={developer_activity} alt="developer" />
                 </div>
-                <div className="MyIntoPar center-flex-class">
-                  <div className="introPar">
-                    <div className="introPar__img">
-                      <div className="profile-pic-card">
-                        <div className="profile-pic-card-inner center-flex-class">
-                          <div className="profile-pic-card-front center-flex-class">
-                            <img src={arnab} alt="arnab" />
-                          </div>
-                          <div className="profile-pic-card-back  center-flex-class">
-                            <div className="introParBody">
-                              <div className="span2 sp1">⚽</div>
-                              <div className="span2 sp2">💻</div>
-                              <div className="span2 sp3">🎨</div>
-                              <div className="span2 sp4">🍔</div>
-                            </div>
-                          </div>
+              </div>
+              <div className="subCLs svg2">
+                <div className="imgPar m center-flex-class">
+                  <img src={solution_mindset} alt="solution_mindset" />
+                </div>
+              </div>
+              <div className="subCLs svg3">
+                <div className="imgPar m center-flex-class">
+                  <img src={Mobile_application} alt="Mobile_application" />
+                </div>
+              </div>
+            </div>
+            <div className="profileDescPart MyIntoPar center-flex-class">
+              <div className="introPar">
+                <div className="introPar__img">
+                  <div className="profile-pic-card">
+                    <div className="profile-pic-card-inner center-flex-class">
+                      <div className="profile-pic-card-front center-flex-class">
+                        <img src={arnab} alt="arnab" />
+                      </div>
+                      <div className="profile-pic-card-back  center-flex-class">
+                        <div className="introParBody">
+                          <div className="span2 sp1">⚽</div>
+                          <div className="span2 sp2">💻</div>
+                          <div className="span2 sp3">🎨</div>
+                          <div className="span2 sp4">🍔</div>
                         </div>
                       </div>
-                      {/* <img src={arnab} alt="arnab" /> */}
                     </div>
-                    <div className="introParHead">
-                      <div className="uname">Arnab Datta</div>
-                      <div className="utxt">
-                        <strong>
-                          Software Developer <span>💻</span>
-                        </strong>
-                      </div>
-                      <div className="loc">
-                        <span className="loc-icon">
-                          <img
-                            className="loc-icon-img"
-                            src={location}
-                            alt="location"
-                          />
-                        </span>
+                  </div>
+                  {/* <img src={arnab} alt="arnab" /> */}
+                </div>
+                <div className="introParHead">
+                  <div className="uname">Arnab Datta</div>
+                  <div className="utxt">
+                    <strong>
+                      Software Developer <span>💻</span>
+                    </strong>
+                  </div>
+                  <div className="loc">
+                    <span className="loc-icon">
+                      <img
+                        className="loc-icon-img"
+                        src={location}
+                        alt="location"
+                      />
+                    </span>
 
-                        <span className="loc-txt">Kolkata, India</span>
-                      </div>
-
-                      {/* <span className="span1">Hey, I'm</span>
-                      <br></br>
-                      <span className="span2">Arnab Datta</span>
-                      <br></br>
-                      <span className="span3">Software Engineer </span>
-                      <span className="span31">💻</span> */}
-                      <div className="span4">
-                        <span className="span4-badges">Web</span>
-                        <span className="span4-badges">Frontend</span>
-                        <span className="span4-badges">Backend</span>
-                        <span className="span4-badges">Mobile Apps</span>
-                      </div>
-                      <div className="span5">
-                        <span className="call">Learn</span>
-                        <span className="col51">|</span>
-                        <span className="col5">|</span>
-                        <span className="call">Grow</span>
-                        <span className="col51">|</span>
-                        <span className="col5">|</span>
-                        <span className="call">Contribute</span>
-                      </div>
-                      {/* <div className="btnAboutMeParent">
+                    <span className="loc-txt">India</span>
+                  </div>
+                  <div className="span4">
+                    <span className="span4-badges">Web</span>
+                    <span className="span4-badges">Frontend</span>
+                    <span className="span4-badges">Backend</span>
+                    <span className="span4-badges">Mobile Apps</span>
+                  </div>
+                  <div className="span5">
+                    <span className="call">Learn</span>
+                    <span className="col51">|</span>
+                    <span className="col5">|</span>
+                    <span className="call">Grow</span>
+                    <span className="col51">|</span>
+                    <span className="col5">|</span>
+                    <span className="call">Contribute</span>
+                  </div>
+                  {/* <div className="btnAboutMeParent">
                         <button className="slide">About Me »</button>
                       </div> */}
-                      <div className="contactCLass">
-                        {/* <h3>Connect me at</h3> */}
-                        <div className="contactCLassChild">
-                          {profilesArr.map((el) => {
-                            return (
-                              <ProfileCards
-                                key={el.id}
-                                obj={el}
-                                hgt={"20px"}
-                                wdt={"20px"}
-                                pdg={"10px"}
-                                bds={"50%"}
-                                mrgn={"5px"}
-                              ></ProfileCards>
-                            );
-                          })}
-                        </div>
-                      </div>
-                      <div className="profile-card-ctr">
-                        <button className="profile-card__button button--blue js-message-btn">
-                          About
-                        </button>
-                        <button className="profile-card__button button--orange">
-                          Follow
-                        </button>
-                      </div>
+                  <div className="contactCLass">
+                    {/* <h3>Connect me at</h3> */}
+                    <div className="contactCLassChild">
+                      {profilesArr.map((el) => {
+                        return (
+                          <ProfileCards
+                            key={el.id}
+                            obj={el}
+                            hgt={"20px"}
+                            wdt={"20px"}
+                            pdg={"10px"}
+                            bds={"50%"}
+                            mrgn={"5px"}
+                          ></ProfileCards>
+                        );
+                      })}
                     </div>
+                  </div>
+                  <div className="profile-card-ctr">
+                    <button className="profile-card__button button--blue js-message-btn">
+                      About
+                    </button>
+                    <button className="profile-card__button button--orange">
+                      Contact
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-            {/* <div className="profilePicSection middleSec">
-              <div className="profileImagePar center-flex-class">
-                <div className="profileImageOuterCircle center-flex-class">
-                  <div className="profileImageDiv">
-                    <div className="profile-pic-card">
-                      <div className="profile-pic-card-inner center-flex-class">
-                        <div className="profile-pic-card-front center-flex-class">
-                          <img src={arnab} alt="arnab" />
-                        </div>
-                        <div className="profile-pic-card-back  center-flex-class">
-                          <div className="introParBody">
-                            <div className="span2 sp1">⚽</div>
-                            <div className="span2 sp2">💻</div>
-                            <div className="span2 sp3">🎨</div>
-                            <div className="span2 sp4">🍔</div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
+            {/* </div> */}
+            {/* </div> */}
             <div className="profileDescPart rightSec">
               <div className="profile-cards-parent">
                 <Link to={"/Skills"}>
